a7ae19bcf81788eb7802040859fa0d801f2f0214
Saima Khan <saimakhancsedu@gmail.com>
2025-08-27 09:25:28 +0600

    Adding  JS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("emergency hotlin");
+const whiteHeart = document.getElementsByClassName("whiteHeart");
+const redHeart = document.getElementById("redHeart");
+let heartCount = 0;
+
+for (const heart of whiteHeart) {
+  heart.addEventListener("click", function () {
+    heartCount++;
+    console.log("heart count" + heartCount);
+  });
+}
+
+console.log(3);
